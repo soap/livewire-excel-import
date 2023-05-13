7870b5d7afd1446c28463ebe2a86733507f4357b
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-05-13 16:39:08 +0700

    Pint updated files

---
+++ src/Http/Livewire/ExcelImporter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Soap\LivewireExcelImport\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class ExcelImporter extends Component
+{
+    use WithFileUploads;
+}
